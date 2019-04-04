af078d98895a70170769fa7a195b966810405145
Mike Milewski <Mike.Milewski.Mail@gmail.com>
2019-04-04 12:45:13 -0400

    ipv4 testing

--- if-test2/iftest2.py
+++ if-test2/iftest2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-ipchk = '192.168.0.1'
+ipchk = input('Apply an IP address: ') # this line prompts the user for input
 
-if ipchk:
-    print('Looks like the IP address was set: ' + ipchk)
+if ipchk == '192.168.1.1': 
+    print('Looks like the Default Gateway IP was set: ' + ipchk) # indented under if
+elif ipchk: # if data is NOT provided
+    print(' Looks like IP address was set')
+else:
+    print('You did not provide input.') # indented under else
